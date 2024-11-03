f6cf452f40efcff2edc5f070008c5f02e3558351
ajid0001 <ajid0001@alginquinlive.com>
2024-11-03 17:19:33 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+void main() async {
+  await fetchAndPrintUserData();
+}
+
+Future<void> fetchAndPrintUserData() async {
+  var url = Uri.parse('https://random-data-api.com/api/v2/users?size=10');
+
+  try {
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var users = convert.jsonDecode(response.body) as List<dynamic>;
+
+      for (var user in users) {
+        final uid = user['uid'];
+        final firstName = user['first_name'];
+        final lastName = user['last_name'];
+
+        print('$uid: $firstName $lastName');
+      }
+    } else {
+      print('Request failed with status: ${response.statusCode}');
+    }
+  } catch (err) {
+    print('An error has occurred: $err');
+  }
+}
